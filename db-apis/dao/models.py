868419a633907ba8b9b6e5df17efa3a1526c6c93
Kunal Rane <kunal.sr@somaiya.edu>
2023-03-15 12:00:00 +0530

    Update primary key in models, added get_data api

--- db-apis/dao/models.py
+++ db-apis/dao/models.py
@@ -13,9 +13,9 @@ class Users(Model):
     repos = columns.List(value_type=columns.Text, required=False)
 
 
+
 class Data(Model):
-    uid = columns.UUID(primary_key=True, default=uuid.uuid4)
     gitid = columns.Text(required=True)
-    name = columns.Text(required=True)
-    repo = columns.Text(required=True)
+    name = columns.Text(primary_key=True,required=True)
+    repo = columns.Text(primary_key=True,required=True)
     commit_num = columns.Integer(required=False, default=0)
