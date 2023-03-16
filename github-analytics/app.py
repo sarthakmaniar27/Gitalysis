f0d267313339c5e7fc00002f34e50d2ac9749c80
Kunal Rane <kunal.sr@somaiya.edu>
2023-03-16 12:00:00 +0530

    Retrieving data from Elastic search

--- github-analytics/app.py
+++ github-analytics/app.py
@@ -1,31 +1,37 @@
 from flask import Flask
-
 from config import config
 from ElasticSearchHelper import ElasticSearchHelper as esh
 from CassandraHelper import CassandraHelper as ch
-
 app = Flask(__name__)
-
 elasticSearchHelper = esh.ElasticSearchHelper()
 cassandraHelper = ch.CassandraHelper()
-
 @app.route('/')
 def home():
     return 'Github Analytics - Use APIs to process data'
 
-
 @app.route('/org/<orgname>')
 def org_retrive(orgname):
-    return elasticSearchHelper.getOrgName(orgname)
+    return elasticSearchHelper.getOrgData(orgname)
 
 @app.route('/repo/<reponame>')
 def repo_retrive(reponame):
-    return reponame
+    return elasticSearchHelper.getRepoData(reponame)
 
 @app.route('/user/<username>')
 def user_retrive(username):
-    return username
+    return elasticSearchHelper.getUserData(username)
 
 
 if __name__ == '__main__':
+
+    
+          
+            
+    
+
+          
+          Expand Down
+    
+    
+  
     app.run(host=config.FLASK_HOST, port=config.FLASK_PORT, debug=True)
