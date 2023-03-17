549550b8ce6f5d72b3c2e809fb9ad5e52632089d
Sarthak Maniar <sarthakmaniar27@gmail.com>
2023-03-17 12:00:00 -0600

    sending each commit to elastic search with server

--- github-crawler/lib/helper.py
+++ github-crawler/lib/helper.py
@@ -3,21 +3,17 @@ import requests
 from pandas.io.json import json_normalize
 from sqlalchemy import create_engine, engine, text, types, MetaData, Table, String
 from datetime import datetime
-
-
 import config
 import os
-
 #elasticsearch
 from elasticsearch import Elasticsearch
 
 GITEA_APP_URL = 'YOUR_GITEA_API'
-GITEA_TOKEN = 'd625eacdf998ccb9b131cf51a7e769358784a546'
-GITHUB_USERNAME = 'cmodi009'
-GITHUB_TOKEN = 'd625eacdf998ccb9b131cf51a7e769358784a546'
+GITEA_TOKEN = 'aff8945f859b696bf05932045f0be7e1b8379ddb'
+GITHUB_USERNAME = 'vishwakulkarni'
+GITHUB_TOKEN = 'aff8945f859b696bf05932045f0be7e1b8379ddb'
 SQL_ALCHEMY_STRING = ''
-
-
+              
 
 class Helper():
     def __init__(self):
@@ -33,13 +29,11 @@ class Helper():
     def set_org_name(self,orgname):
         self.orgname = orgname
         return
-
     def get_org_information(self,owner,api):
         url = api + '/orgs/{}'.format(self.orgname)
         org_data = self.gh_session.get(url = url)
         org_data=json.loads(org_data.content)
         return org_data
-
     def send_to_elasticInstance(self,data,index_name,id_val):
         self.es.index(index=index_name, doc_type='_doc',id=id_val, body=data)
     
@@ -75,17 +69,39 @@ class Helper():
                     print(i)
                     next = False
             i = i + 1
-        
+
         return members_list
+
+    #save commits of repos as json
+    def commits_of_repo_github(self,repo, owner, api):
+        commits = []
+        next = True
+        i = 1
+        while next == True:
+            url = api + '/repos/{}/{}/commits?page={}&per_page=100'.format(owner, repo, i)
+            commit_pg = self.gh_session.get(url = url)
+            commit_tp = json.loads(commit_pg.content)
+            for commit in commit_tp:
+                commits.append(commit) 
+                print(commit)
+            if 'Link' in commit_pg.headers:
+                if 'rel="next"' not in commit_pg.headers['Link']:
+                    next = False
+            i = i + 1
+        return commits
+
 
 
 
 #testing comment after use
 h = Helper()
 github_api = "https://api.github.com"
-h.set_org_name("mozilla")
-#print(h.get_org_information("cmodi009",github_api))
-#k=h.get_repositories('cmodi009',github_api)
+h.set_org_name("CUBigDataClass")
+#print(h.get_org_information("vishwakulkarni",github_api))
+k=h.get_repositories('vishwakulkarni',github_api)
+commits = h.commits_of_repo_github('kode-kallas','cubigdataclass',github_api)
+for commit in commits:
+    h.send_to_elasticInstance(commit,'commit',commit['sha'])
 #print(len(k))
-#for mem in k:
-#    print(mem['name'])
+for mem in k:
+    print(mem['name'])
