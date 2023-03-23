185efb5f312965ce58decd7ae178194daaa90fa1
Kunal Rane <kunal.sr@somaiya.edu>
2023-03-23 12:00:00 +0530

    Updated missing thread and created sampleJson

--- github-analytics/CassandraHelper/config.py
+++ github-analytics/CassandraHelper/config.py
@@ -1,10 +1,13 @@
 CASSANDRA_HOST = 'localhost'
 CASSANDRA_PORT = 5000
-
 CASSANDRA_URL = "http://" + CASSANDRA_HOST + ":" + str(CASSANDRA_PORT) + "/"
-
 CASSANDRA_URL_INSERT = CASSANDRA_URL + "insert/"
 
 # Github Config
 GITHUB_USER = "cmodi009"
 GITHUB_TOKEN = "f02831928492a6bfe9f1bca49693f69d1d0e44b3"
+
+
+# Others
+
+THREAD_COUNT = 10
