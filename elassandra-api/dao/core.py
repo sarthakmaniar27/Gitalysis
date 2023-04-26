2e11099d0eda314c901c922a41ba60a6ac4dfb89
Sarthak Maniar <sarthakmaniar27@gmail.com>
2023-04-26 12:00:00 -0600

    Updated crawler

--- elassandra-api/dao/core.py
+++ elassandra-api/dao/core.py
@@ -1,16 +1,23 @@
 import json
 import time
+
 from cassandra.cluster import Cluster
 from cassandra.cqlengine import connection
 from cassandra.query import SimpleStatement, BatchStatement
+
 from .config import CASSANDRA_HOSTS
 from flask import jsonify
+
+
 ORG = "org"
 TABLE = "table"
 BODY = "body"
 START_ID = "start_id"
 END_ID = "end_id"
 USERS = "users"
+
+
+
 # TODO: Enable quorum
 def insert(request):
     # TODO: Data Validation should be done?
@@ -34,8 +41,6 @@ def insert(request):
         ans = json.dumps(obj)
         query = SimpleStatement("INSERT INTO " + content[TABLE] + " JSON \'" + ans + "\';")
         #batch.add(query)
-    
-  
         try:
             session.execute(query)
         except Exception as e:
@@ -48,6 +53,7 @@ def insert(request):
     session.shutdown()
     # Repo.create(obj)
     return obj
+
 def insert_commit(request):
     # TODO: Data Validation should be done?
     content = request.get_json()
@@ -72,6 +78,8 @@ def insert_commit(request):
     session.shutdown()
         #Repo.create(obj)
     return obj
+
+
 def get_data(request):
     content = request.get_json()
     connection.setup(CASSANDRA_HOSTS, content[ORG])
@@ -80,6 +88,8 @@ def get_data(request):
     for o in obj:
         obj_list.append(dict(o))
     return jsonify(obj_list)
+
+
 def get_all_data(request):
     content = request.get_json()
     connection.setup(CASSANDRA_HOSTS, content[ORG])
